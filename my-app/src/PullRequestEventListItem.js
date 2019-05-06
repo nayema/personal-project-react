92380efd4bed3052488fa00c4c84f9c3a21ffba0
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-05 22:24:20 -0400

    Add pull request state

--- my-app/src/PullRequestEventListItem.js
+++ my-app/src/PullRequestEventListItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const PullRequestEventListItem = ({event}) => (
-  <a key={event.payload.pull_request.id} href={`${event.payload.pull_request.html_url}`}>
-    <li key={event.repo.id}
-    >
-      {event.repo.name}
-    </li>
-  </a>
+  <div>
+    <a key={event.payload.pull_request.id} href={`${event.payload.pull_request.html_url}`}>
+      <li key={event.repo.id}
+      >
+        {event.repo.name}
+      </li>
+    </a>
+    Status: {event.payload.pull_request.state}
+  </div>
 );
 
 export default PullRequestEventListItem;
