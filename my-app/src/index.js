6834c9eff10ca568d4d7d250b004c9e559b22244
Nayema Nasir <nayema.nasir89@gmail.com>
2019-05-11 16:45:37 -0400

    Update redux devtools extension

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,7 +17,7 @@ const middlewares = [
 ];
 
 if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.devToolsExtension;
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
   if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension())
